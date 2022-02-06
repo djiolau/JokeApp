93d398245089cbc869642f1ddba3f6585419e8e8
Patrick <83156379+djiolau@users.noreply.github.com>
2022-02-06 22:28:40 +0100

    final commit

---
+++ src/main/java/project/jokeapp/sources/config/ChuckConfiguration.java
@@ -0,0 +1,8 @@
+package project.jokeapp.sources.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChuckConfiguration {
+
+}
---
+++ src/main/java/project/jokeapp/sources/services/JokeService.java
@@ -0,0 +1,5 @@
+package project.jokeapp.sources.services;
+
+public interface JokeService {
+    String getJoke();
+}
